54a9d8c2f1c2a486d1dc40e510e06d99d48af927
ghoolio <david.noeldner@icloud.com>
2024-01-05 20:08:45 +0100

    first commit

---
+++ app/components/AnnouncementBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnnouncementBar = () => {
+    return (
+        <div className="bg-white text-center p-2 font-montserrat underline text-red-500 font-bold">
+            <h1>Let's go</h1>
+        </div>
+    )
+}
+
+export default AnnouncementBar;
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import HeaderNavbar from './components/HeaderNavbar.jsx'
+import HeroSection from './components/HeroSection'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <HeaderNavbar />
+      <HeroSection />
+    </div>
+  )
+}

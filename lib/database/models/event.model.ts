e836c6894d57eea743fc5318203080969c4f2feb
ghoolio <david.noeldner@icloud.com>
2024-01-16 12:49:36 +0100

    type interface for envent models

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,20 @@
-import { Schema, model, models } from 'mongoose';
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string, name: string }
+  organizer: { _id: string, firstName: string, lastName: string }
+}
 
 const EventSchema = new Schema({
   title: { type: String, required: true },
@@ -13,7 +29,7 @@ const EventSchema = new Schema({
   url: { type: String },
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
   organizer: { type: Schema.Types.ObjectId, ref: 'User' },
-});
+})
 
 const Event = models.Event || model('Event', EventSchema);
 
